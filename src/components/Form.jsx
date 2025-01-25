99ee530156ad5efb783738825220eb8780bb56c2
ThuraMinThein <thur7012@gmail.com>
2025-01-25 23:30:42 +0630

    change form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,52 +1,33 @@
 import { useRef } from "react"
-import { useApp } from "../ThemedApp";
+import { Box, TextField, Button } from "@mui/material";
 
 export default function Form({ add }) {
 
-    const { mode } = useApp();
     const contentRef = useRef();
-    const nameRef = useRef();
 
     return (
         <form
             onSubmit={e => {
                 e.preventDefault();
                 const content = contentRef.current.value;
-                const name = nameRef.current.value;
-                add(content, name);
+                add(content, "James");
                 e.currentTarget.reset();
-            }}
-
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                gap: 3,
-                padding: 10,
-                borderRadius: 8,
-                marginBottom: 20,
-                background: mode === "dark" ? "#555" : "#def",
             }}>
-            <input
-                ref={contentRef}
-                type="text"
-                placeholder="Content"
-                style={{ padding: 5 }}
-            />
-            <input
-                ref={nameRef}
-                type="text"
-                placeholder="Name"
-                style={{ padding: 5 }}
-            />
-            <button
-                type="submit"
-                style={{
-                    padding: 8,
-                    background: "#0d6efd",
-                    color: "white",
-                    border: "0 none",
-                }}
-            >Post</button>
+            <Box sx={{ mb: 4, textAlign: "right" }}>
+                <TextField
+                    inputRef={contentRef}
+                    type="text"
+                    placeholder="Content"
+                    fullWidth
+                    multiline
+                    sx={{ mb: 1 }}
+                />
+                <Button
+                    variant="contained"
+                    type="submit">
+                    Post
+                </Button>
+            </Box>
         </form>
     )
 }
