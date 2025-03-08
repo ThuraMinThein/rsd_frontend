5169f86af8e3080e38065528ec10dca344129c26
ThuraMinThein <thur7012@gmail.com>
2025-03-08 22:26:10 +0630

    add like unlike post and comment api call

--- src/libs/fetcher.jsx
+++ src/libs/fetcher.jsx
@@ -1,3 +1,5 @@
+import { getToken } from "../auth/auth-service";
+
 const base_url = import.meta.env.VITE_BASE_URL;
 
 export async function createUser(data) {
@@ -32,4 +34,76 @@ export async function loginUser(userName, password) {
         throw new Error("Incorrect Credentials");
     }
 
+}
+
+export async function likePost(data) {
+    const token = "Bearer " + getToken();
+    const res = await fetch(`${base_url}/posts/like`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json",
+            "authorization": token
+        },
+        body: JSON.stringify(data),
+    });
+    if (res.ok) {
+        return await res.json();
+    } else {
+        throw new Error(res.statusText);
+    }
+}
+
+export async function unlikePost(data) {
+    const token = "Bearer " + getToken();
+    const res = await fetch(`${base_url}/posts/unlike`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json",
+            "authorization": token
+        },
+        body: JSON.stringify(data),
+    });
+    if (res.ok) {
+        return await res.json();
+    } else {
+        throw new Error(res.statusText);
+    }
+}
+
+export async function likeComment(data) {
+    const token = "Bearer " + getToken();
+    const res = await fetch(`${base_url}/comments/like`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json",
+            "authorization": token
+        },
+        body: JSON.stringify(data),
+    });
+    if (res.ok) {
+        return await res.json();
+    } else {
+        throw new Error(res.statusText);
+    }
+}
+
+export async function unlikeComment(data) {
+    const token = "Bearer " + getToken();
+    const res = await fetch(`${base_url}/comments/unlike`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json",
+            "authorization": token
+        },
+        body: JSON.stringify(data),
+    });
+    if (res.ok) {
+        return await res.json();
+    } else {
+        throw new Error(res.statusText);
+    }
 }
